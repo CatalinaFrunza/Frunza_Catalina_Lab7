fb77c7665d386f8f3a600a48d8f8b99915fdb35a
Catalina Frunza <116273388+CatalinaFrunza@users.noreply.github.com>
2022-11-30 14:04:33 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Frunza_Catalina_Lab7;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Frunza_Catalina_Lab7;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
